c3bfd55bae1231ecb8a750cbc33dd7273e82c3e7
Julio Treviño Páez <julio.tpaez@gmail.com>
2022-06-24 13:51:25 +0200

    Make pubkey input accept null values and fix airdrop to start empty

--- website/src/components/general/input/PubkeyInput.vue
+++ website/src/components/general/input/PubkeyInput.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import {PublicKey} from '@solana/web3.js';
 
-defineProps<{
-    modelValue: PublicKey;
+const props = defineProps<{
+    modelValue: PublicKey | null; acceptNull?: boolean
 }>();
 const emits = defineEmits<{
-    (e: 'update:model-value', value: PublicKey): void,
+    (e: 'update:model-value', value: PublicKey | null): void,
 }>();
 
 // REFS -----------------------------------------------------------------------
@@ -13,11 +13,20 @@ const emits = defineEmits<{
 // METHODS --------------------------------------------------------------------
 
 function onUpdate(value: string) {
-    try {
-        const pubkey = new PublicKey(value);
-        emits('update:model-value', pubkey);
-    } catch (e) {
-        // ignore
+    if (props.acceptNull) {
+        try {
+            const pubkey = new PublicKey(value);
+            emits('update:model-value', pubkey);
+        } catch (e) {
+            emits('update:model-value', null);
+        }
+    } else {
+        try {
+            const pubkey = new PublicKey(value);
+            emits('update:model-value', pubkey);
+        } catch (e) {
+            // ignore
+        }
     }
 }
 
@@ -26,7 +35,12 @@ function onUpdate(value: string) {
 </script>
 
 <template>
-    <q-input v-bind="$attrs" :model-value="modelValue.toString()" @update:model-value="onUpdate" outlined dense>
+    <q-input v-bind="$attrs"
+             :model-value="modelValue?.toString() ?? ''"
+             @update:model-value="onUpdate"
+             outlined
+             dense
+             placeholder="Solana Address">
         <template v-for="(_, slot) of $slots" v-slot:[slot]="scope">
             <slot :name="slot" v-bind="scope ?? {}"/>
         </template>
