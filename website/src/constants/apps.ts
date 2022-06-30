047a49da61c1cd573691525064106b923160f184
Julio Treviño Páez <julio.tpaez@gmail.com>
2022-06-30 12:51:56 +0200

    Move tools page to the top

--- website/src/constants/apps.ts
+++ website/src/constants/apps.ts
@@ -1,6 +1,11 @@
 import {DrawerAppButton} from 'src/types/drawer';
 
 export const APP_BUTTONS: DrawerAppButton[] = [{
+    icon: 'fa-solid fa-screwdriver-wrench',
+    name: 'Tools',
+    description: 'Select the tool you need',
+    pathName: 'Tools',
+}, {
     icon: 'fa-solid fa-code-commit',
     name: 'Transaction Builder',
     description: 'Build custom transactions mixing different instructions',
@@ -11,11 +16,6 @@ export const APP_BUTTONS: DrawerAppButton[] = [{
     description: 'Inspect and manage the list of available programs to work with',
     pathName: 'ProgramExplorer',
     pathAlias: ['Program'],
-}, {
-    icon: 'fa-solid fa-screwdriver-wrench',
-    name: 'Tools',
-    description: 'Select the tool you need',
-    pathName: 'Tools',
 }, {
     icon: 'fa-solid fa-gear',
     name: 'Settings',
